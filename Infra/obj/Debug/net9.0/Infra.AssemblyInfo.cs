2942897cac0a9f3214116b92e2db661940c604b1
raja <rashok-adm@transportforlondon.onmicrosoft.com>
2026-06-12 22:49:56 +0100

    changes

--- Infra/obj/Debug/net9.0/Infra.AssemblyInfo.cs
+++ Infra/obj/Debug/net9.0/Infra.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infra")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e1fdc74aad6e48ad84b37bd3ef25918a24b31eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infra")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infra")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
